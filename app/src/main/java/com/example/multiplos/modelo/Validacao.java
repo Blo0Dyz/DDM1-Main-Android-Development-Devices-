6bfdd231aa3c1866cad05d5c4c58e5504ca06299
Marcio Claudino <m.antonio.cs00@gmail.com>
2026-04-15 21:27:45 -0300

    2 Operation functions added, returning the result as a message to user.

--- app/src/main/java/com/example/multiplos/modelo/Validacao.java
+++ app/src/main/java/com/example/multiplos/modelo/Validacao.java
@@ -1,4 +1,19 @@
 package com.example.multiplos.modelo;
 
-public class Validacao {
+public class Validacao
+{
+    public Integer num;
+    public String validarNumero(String numero)
+    {
+        String mensagem = "";
+        try
+        {
+            num = Integer.parseInt(numero);
+
+        } catch (Exception e) {
+            mensagem = "Digite valores validos";
+
+        }
+        return mensagem;
+    }
 }
